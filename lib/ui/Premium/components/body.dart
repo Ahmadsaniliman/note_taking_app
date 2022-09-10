fe0730eb590461c0cdb835f5264d8c30464b1b10
Ahmadsaniliman <saniahmadliman@gmail.com>
2022-09-10 09:08:25 +0100

    make some changes

--- lib/ui/Premium/components/body.dart
+++ lib/ui/Premium/components/body.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note_taking_app/defaultButton/default_button.dart';
 import 'package:note_taking_app/ui/Premium/components/desc_text.dart';
-import 'package:note_taking_app/ui/Premium/components/sub_container.dart';
 import 'package:note_taking_app/ui/Premium/components/title_row.dart';
 
 class Body extends StatelessWidget {
@@ -9,6 +8,47 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // int _currentOne = 0;
+    final List<Widget> data = [
+      Column(
+        children: const [
+          Text('Annaul'),
+          Padding(
+            padding: EdgeInsets.only(
+              top: 10.00,
+              bottom: 15.0,
+            ),
+            child: Text(
+              '\$76.99',
+              style: TextStyle(
+                fontSize: 15.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Text('per year'),
+        ],
+      ),
+      Column(
+        children: const [
+          Text('Monthly'),
+          Padding(
+            padding: EdgeInsets.only(
+              top: 10.00,
+              bottom: 15.0,
+            ),
+            child: Text(
+              '\$7.99',
+              style: TextStyle(
+                fontSize: 15.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Text('per month'),
+        ],
+      ),
+    ];
     return Column(
       children: [
         const SizedBox(height: 30.0),
@@ -16,9 +56,33 @@ class Body extends StatelessWidget {
         const DescText(),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: const [
-            SubContainer(),
-            SubContainer(),
+          children: [
+            ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (context, index) => GestureDetector(
+                onTap: () {},
+                child: Container(
+                  padding: const EdgeInsets.all(15.0),
+                  height: 140,
+                  width: 140,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(
+                      color: const Color(0xFFD9614C),
+                      width: 7,
+                    ),
+                    borderRadius: BorderRadius.circular(
+                      15.0,
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      Text('${data[index]}'),
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
         const SizedBox(height: 20.0),
--- lib/ui/Premium/components/sub_container.dart
+++ lib/ui/Premium/components/sub_container.dart
@@ -1,44 +1,57 @@
 import 'package:flutter/material.dart';
 
-class SubContainer extends StatelessWidget {
+class SubContainer extends StatefulWidget {
   const SubContainer({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<SubContainer> createState() => _SubContainerState();
+}
+
+class _SubContainerState extends State<SubContainer> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15.0),
-      height: 140,
-      width: 140,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border.all(
-          color: const Color.fromARGB(255, 247, 220, 220),
-          width: 7,
+    int index = 0;
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          index = index;
+        });
+      },
+      child: Container(
+        padding: const EdgeInsets.all(15.0),
+        height: 140,
+        width: 140,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(
+            color: index == index ? const Color(0xFFD9614C) : Colors.green,
+            width: 7,
+          ),
+          borderRadius: BorderRadius.circular(
+            15.0,
+          ),
         ),
-        borderRadius: BorderRadius.circular(
-          15.0,
-        ),
-      ),
-      child: Column(
-        children: const [
-          Text('Annaul'),
-          Padding(
-            padding: EdgeInsets.only(
-              top: 10.00,
-              bottom: 15.0,
-            ),
-            child: Text(
-              '\$76.99',
-              style: TextStyle(
-                fontSize: 15.0,
-                fontWeight: FontWeight.bold,
+        child: Column(
+          children: const [
+            Text('Annaul'),
+            Padding(
+              padding: EdgeInsets.only(
+                top: 10.00,
+                bottom: 15.0,
+              ),
+              child: Text(
+                '\$76.99',
+                style: TextStyle(
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-          Text('per year'),
-        ],
+            Text('per year'),
+          ],
+        ),
       ),
     );
   }
