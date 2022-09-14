9e4d4065bdea27422564dbbd121625ba853e6af3
Ahmadsaniliman <saniahmadliman@gmail.com>
2022-09-14 08:50:11 +0100

    make some changes

--- lib/ui/Note/recent_note.dart
+++ lib/ui/Note/recent_note.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:note_taking_app/ui/Note/components/recent_build_app_bar.dart';
-import 'package:note_taking_app/ui/Note/components/recent_note_data.dart';
 
 class RecentNoteView extends StatelessWidget {
   const RecentNoteView({Key? key}) : super(key: key);
@@ -15,43 +14,187 @@ class RecentNoteView extends StatelessWidget {
           vertical: 20.0,
           horizontal: 25.0,
         ),
-        child: GridView.builder(
-          itemCount: recentNoteData.length,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisSpacing: 20,
-              mainAxisSpacing: 20.0,
-              crossAxisCount: 2,
-              childAspectRatio: 0.75),
-          itemBuilder: (context, index) => Container(
-            // height: 200,
-            // width: 150,
-            color: Colors.green,
-            child: Text(
-              recentNoteData[index],
+        child: Column(
+          children: [
+            Row(
+              children: const [
+                FirstText(),
+                SecondColumn(),
+              ],
             ),
-          ),
+          ],
         ),
       ),
     );
   }
 }
 
+class SecondColumn extends StatelessWidget {
+  const SecondColumn({
+    Key? key,
+  }) : super(key: key);
 
-// GridView.builder(
-//             itemCount: recentNoteData.length,
-//             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-//               childAspectRatio: 0.75,
-//               crossAxisCount: 2,
-//               crossAxisSpacing: 20.0,
-//               mainAxisSpacing: 20.0,
-//             ),
-//             itemBuilder: (context, index) => Container(
-              
-//                 decoration: BoxDecoration(
-//               color: Colors.green,
-//                   borderRadius: BorderRadius.circular(15.0),
-//                 ),
-//                 child: Text(recentNoteData[index]),
-//             ),
-//           ),
-//         ),
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: 150.0,
+          padding: const EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Column(
+            children: const [
+              Text(
+                'Trends List',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15.0,
+                ),
+              ),
+              Text('* Glassmorphism'),
+              Text('* Claymorphism'),
+              Text('* Big Typography'),
+              Text('* 3d Illustrations'),
+              Text('n* Flat Colors...'),
+            ],
+          ),
+        ),
+        Container(
+          width: 150.0,
+          padding: const EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Column(
+            children: const [
+              Text(
+                'School Essay on Accessibility',
+                style: TextStyle(
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Accessibility is the practice of making your websites usable by as many people as possible. We traditionally think of this as being about people with disabilities...',
+              ),
+            ],
+          ),
+        ),
+        Container(
+          width: 150.0,
+          padding: const EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Column(
+            children: [
+              const Text(
+                'Groceries',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15.0,
+                ),
+              ),
+              Row(
+                children: const [
+                  Icon(
+                    Icons.check_box_outline_blank,
+                  ),
+                  Text('2x Apples'),
+                ],
+              ),
+              Row(
+                children: const [
+                  Icon(
+                    Icons.check_box_outline_blank,
+                  ),
+                  Text('6x Eggs'),
+                ],
+              ),
+              Row(
+                children: const [
+                  Icon(
+                    Icons.check_box_outline_blank,
+                  ),
+                  Text('1x Broccoli'),
+                ],
+              ),
+              Row(
+                children: const [
+                  Icon(
+                    Icons.check_box_outline_blank,
+                  ),
+                  Text('5x Maggie L'),
+                ],
+              ),
+              Row(
+                children: const [
+                  Icon(
+                    Icons.check_box_outline_blank,
+                  ),
+                  Text('2x Cupcakes'),
+                ],
+              ),
+              Row(
+                children: const [
+                  Icon(
+                    Icons.check_box_outline_blank,
+                  ),
+                  Text('3x Ball Pens'),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class FirstText extends StatelessWidget {
+  const FirstText({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150.0,
+      padding: const EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Column(
+        children: const [
+          Text(
+            'Cerebral palsy sport',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 15.0,
+            ),
+          ),
+          Text(
+            'Cerebral palsy sport classification is a classification system used by sports that include people with cerebral palsy (CP) with different degrees of severity to compete fairly against each...',
+          ),
+          SizedBox(height: 20.0),
+          Text(
+            'Emotions of Typography',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 15.0,
+            ),
+          ),
+          Text(
+            'Typography is a part of user interface. Many of us focus on User Experience rather than User Interface as a novice UI/UX designer since deciding a typeface, colors, and typefaces is a pain as it takes a lot of...',
+          ),
+        ],
+      ),
+    );
+  }
+}
