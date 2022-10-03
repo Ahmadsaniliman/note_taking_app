706eb0b7c3530657961c935efc67b2025e128092
Ahmadsaniliman <saniahmadliman@gmail.com>
2022-10-03 23:24:11 +0100

    another commit

--- lib/ui/Note/newNote/new_note_view.dart
+++ lib/ui/Note/newNote/new_note_view.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:note_taking_app/ui/CreateAccount/components/name_text_field.dart';
+import 'package:note_taking_app/util/util.dart';
+import 'dart:developer' as devtool show log;
 
 class NewNoteView extends StatefulWidget {
   const NewNoteView({Key? key}) : super(key: key);
@@ -26,11 +29,34 @@ class _NewNoteViewState extends State<NewNoteView> {
     super.dispose();
   }
 
+  final note = FirebaseFirestore.instance.collection('notes');
+//   final userTexts = <String, dynamic>{
+//     'userTitle': '',
+//     'userText': '',
+//   };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: IconButton(
-        onPressed: () {},
+        onPressed: () async {
+          if (_titleController.text.isEmpty && _textController.text.isEmpty) {
+            await showErrorAlertDialog(
+              context: context,
+              contentText:
+                  'Title Or Content Field Is Empty Please Input a Text',
+            );
+          } else {
+            note.add({
+              'userTitle': _titleController.text,
+              'userContent': _textController.text,
+            }).then((response) {
+              devtool.log(response.id);
+              _titleController.clear();
+              _textController.clear();
+            });
+          }
+        },
         icon: const Icon(
           Icons.save,
         ),
