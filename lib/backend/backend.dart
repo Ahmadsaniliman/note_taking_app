dafdda4fc80b4cd2bea70593718efc0aaeade10d
Ahmadsaniliman <saniahmadliman@gmail.com>
2022-10-08 08:14:32 +0100

    another commit

--- lib/backend/backend.dart
+++ lib/backend/backend.dart
@@ -1,9 +1,56 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+// ignore_for_file: equal_keys_in_map
 
-class CloudNote {
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:note_taking_app/backend/cloud_note.dart';
+import 'package:note_taking_app/backend/user_text.dart';
+
+class DbStorage {
   final dbNotes = FirebaseFirestore.instance.collection('notes');
 
-  CloudNote._sharedInstance();
-  static final CloudNote shared = CloudNote._sharedInstance();
-  factory CloudNote() => shared;
+  DbStorage._sharedInstance();
+  static final DbStorage shared = DbStorage._sharedInstance();
+  factory DbStorage() => shared;
+
+  Future<CloudNote> createNote() async {
+    try {
+      final newNote = await dbNotes.add({
+        userTitle: '',
+        // ignore: equal_keys_in_map
+        userContent: '',
+      });
+
+      await newNote.get();
+      return CloudNote(
+        title: '',
+        contentText: '',
+      );
+    } catch (e) {
+      throw CouldNotCReateNoteException();
+    }
+  }
+
+  Future<void> deleteNote() async {
+    try {
+      await dbNotes.doc().delete();
+    } catch (e) {
+      throw CouldNotDeleteNoteException();
+    }
+  }
+
+  Future<void> updateNote() async {
+    try {
+      await dbNotes.doc().update({
+        userTitle: '',
+        userContent: '',
+      });
+    } catch (e) {
+      throw CouldNotUpdateNoteException();
+    }
+  }
 }
+
+class CouldNotCReateNoteException implements Exception {}
+
+class CouldNotDeleteNoteException implements Exception {}
+
+class CouldNotUpdateNoteException implements Exception {}
--- lib/backend/cloud_note.dart
+++ lib/backend/cloud_note.dart
@@ -1,16 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:note_taking_app/backend/user_text.dart';
 
-class Note {
+class CloudNote {
   final String title;
   final String contentText;
 
-  Note({
+  CloudNote({
     required this.title,
     required this.contentText,
   });
 
-  Note.fromSnapshot(QueryDocumentSnapshot<Map<String, dynamic>> snapshot)
+  CloudNote.fromSnapshot(QueryDocumentSnapshot<Map<String, dynamic>> snapshot)
       : title = snapshot.data()[userTitle],
         contentText = snapshot.data()[userContent];
 }
